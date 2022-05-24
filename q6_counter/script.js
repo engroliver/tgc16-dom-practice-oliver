dbf1c88f75c27e9dbbea3afef381f8544db2eecd
engroliver <engr.oliver@icloud.com>
2022-05-24 13:28:01 +0000

    dom q6

---
+++ q6_counter/script.js
@@ -0,0 +1,49 @@
+let dec = document.querySelector('#decrement');
+let inc = document.querySelector('#increment');
+let res = document.querySelector('#reset');
+let box = document.querySelector('#box');
+let n = 0
+box.style.backgroundColor="green"
+
+inc.addEventListener('click', function(){
+ 
+if (box.innerHTML < 10){
+    box.innerHTML = parseInt(box.innerHTML) + 1;
+}
+else{
+    box.innerHTML.disable = true
+}
+
+if (box.innerHTML % 2 == 0){
+    box.style.backgroundColor = 'green';
+}
+else { 
+    box.style.backgroundColor = 'red';
+}
+})
+
+dec.addEventListener('click', function(){
+ 
+    if (box.innerHTML > 0){
+        box.innerHTML = parseInt(box.innerHTML) - 1;
+    }
+    else{
+        box.innerHTML.disable = true
+    }
+    
+    if (box.innerHTML % 2 == 0){
+        box.style.backgroundColor = 'green';
+    }
+    else { 
+        box.style.backgroundColor = 'red';
+    }
+    })
+
+
+res.addEventListener('click', function(){
+    box.innerHTML = 0
+            
+})
+
+
+
